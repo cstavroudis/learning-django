b522026ee8a25f726c25176902f2daa3497afd5f
Long Phan <long.phan@longs-mbp.mynetworksettings.com>
2022-06-17 11:38:53 -0400

    bad commit msg

--- manage.py
+++ manage.py
@@ -3,11 +3,12 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'composeexample.settings')
     try:
+        print("hello world1111")
+
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
